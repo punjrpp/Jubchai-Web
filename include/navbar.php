239de27a81f3d38e3b7ba7464c4168626f656103
punjrpp <83388590+punjrpp@users.noreply.github.com>
2021-05-04 23:49:54 +0700

    include files
    include files

---
+++ include/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+      <ul class="menu">
+        <li class="Logo"><a href="page.php"><img width="70px" src="../img/jccom.png" alt="jccomlogo"></a></li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <li class="item fas fa-home"><a href="page.php"> หน้าหลัก</a></li>
+        <li class="item fas fa-info-circle"><a href="howtoplay.php"> วิธีการเล่น</a></li>
+        <li class="item fas fa-user-shield"><a href="member.php"> ทีมงาน</a></li>
+        <!-- <li class="item fas fa-id-card-alt"><a href="#"> ติอต่อ</a></li> -->
+        <li class="item button secondary"><a href="../logout.php">Logout</a></li>
+        <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+      </ul>
+    </nav>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script sec="assets/js/script.js"></script>
+    <br>
+
+
---
+++ include/slider.php
@@ -0,0 +1,64 @@
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+<!--   <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol> -->
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox" style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+    <div class="item active">
+      <img width="1440px" src="../img/ปก.png" alt="..." style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+    <div class="item">
+      <img width="1440px" src="../img/rulesl.jpg" alt="..." style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+    <div class="item">
+      <img width="1440px" src="../img/htpsl.jpg" alt="..." style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="
+              border-top-left-radius: 20px;
+              border-bottom-left-radius: 20px;
+              border-top-right-radius: 20px;
+              border-bottom-right-radius: 20px;">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
